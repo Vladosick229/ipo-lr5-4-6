e99b803b115edb0f45e5e4f58479d609bd109a0e
Vlad Sevruk <vladworking5@gmail.com>
2024-10-15 22:29:31 +0300

    №6

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-with open('text.txt', 'r') as infile:
-    spis = infile.readlines()
+with open('text.txt', 'r', encoding='utf-8') as infile:#чтение информации из файла text.txt
+    spis = infile.readlines()#инициализация переменной spis
 
-with open('output.txt', 'w') as outfile:
-    for line in spis:
-        outfile.write(line[::-1] + '\n')
+with open('output.txt', 'w', encoding='utf-8') as outfile:#запись информации в файл output.txt
+    for line in spis:#цикл for
+        outfile.write(line[::-1] + '\n')#запись строки в обратном порядке
